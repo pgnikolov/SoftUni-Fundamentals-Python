e4b7894d028e5726077f57984ddf8a98c7ff3cf0
pgnikolov <151896883+pgnikolov@users.noreply.github.com>
2024-04-07 17:43:03 +0200

    Create 01_concat_names.py

---
+++ 04_Data_Types_and_Variables_Lab /01_concat_names.py
@@ -0,0 +1,7 @@
+# Write a program that reads two names and a delimiter. It should print the names joined by the delimiter.
+
+name_1 = input()
+name_2 = input()
+delimiter = input()
+
+print(f"{name_1}{delimiter}{name_2}")
