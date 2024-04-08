158413889601228c4d2aac43fcfd1e784ff5f825
pgnikolov <151896883+pgnikolov@users.noreply.github.com>
2024-04-08 12:56:26 +0200

    Create 03_elevator.py

---
+++ 05_Data_Types_and_Variables_Exercise /03_elevator.py
@@ -0,0 +1,12 @@
+# Calculate how many courses will be needed to elevate N persons by using an elevator with a capacity of P persons.
+# The input holds two lines: the number of people N and the capacity P of the elevator.
+
+n = int(input())
+p = int(input())
+
+if n <= p:
+    print("1")
+elif n > p and n % p == 0:
+    print(n // p)
+else:
+    print(n // p + 1)
