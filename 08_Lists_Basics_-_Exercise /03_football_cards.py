4c1c47b621dd7d12cbc4d3365b906e542f254f28
pgnikolov <151896883+pgnikolov@users.noreply.github.com>
2024-04-09 21:40:36 +0200

    Update 03_football_cards.py

--- 08_Lists_Basics_-_Exercise /03_football_cards.py
+++ 08_Lists_Basics_-_Exercise /03_football_cards.py
@@ -8,30 +8,6 @@
 # remaining players on each team at the end of the game :"Team A - {players_count};Team B - {players_count}".
 # If the referee terminated the game, print an additional line: "Game was terminated".
 
-# A = list(range(1, 12))
-# B = list(range(1, 12))
-#
-# sequence_cards = input()
-# list_cards = sequence_cards.split(" ")
-# not_terminated = True
-#
-# teamA = ['A-' + str(char) for char in A]
-# teamB = ['B-' + str(char) for char in B]
-#
-# for i in range(len(list_cards)):
-#     if list_cards[i] in teamA:
-#         teamA.remove(list_cards[i])
-#     elif list_cards[i] in teamB:
-#         teamB.remove(list_cards[i])
-#     if len(teamA) < 7 or len(teamB) < 7:
-#         not_terminated = False
-#
-# if not not_terminated:
-#     print(f"Team A - {len(teamA)}; Team B - {len(teamB)}")
-#     print("Game was terminated")
-# else:
-#     print(f"Team A - {len(teamA)}; Team B - {len(teamB)}")
-
 string_of_cards = input().split()
 a_cards = []
 b_cards = []
@@ -51,4 +27,3 @@ if redcards_b > 5:
 print(f'Team A - {11 - redcards_a}; Team B - {11 - redcards_b}')
 if redcards_a == 5 or redcards_b == 5:
     print('Game was terminated')
-    
