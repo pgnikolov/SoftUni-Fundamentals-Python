c44ecc300e1365c23286844fb369930eb687195b
pgnikolov <151896883+pgnikolov@users.noreply.github.com>
2024-04-10 12:21:16 +0200

    Update 01_invert_values.py

--- 08_Lists_Basics_-_Exercise /01_invert_values.py
+++ 08_Lists_Basics_-_Exercise /01_invert_values.py
@@ -11,5 +11,14 @@ for i in range(len(list_nums)):
     elif list_nums[i] < 0:
         list_nums[i] = abs(list_nums[i])
 
-
 print(list_nums)
+
+# Solution 2
+#
+# list_with_numbers = input().split()
+# opposite_numbers = []
+#
+# for number in list_with_numbers:
+#     opposite_number = -int(number)
+#     opposite_numbers.append(opposite_number)
+# print(opposite_numbers)
