43d5795d6a6d38ef81fcc643a0998ba0406fb2bd
pgnikolov <151896883+pgnikolov@users.noreply.github.com>
2024-04-11 20:59:01 +0200

    Update 07_easter_gifts.py

--- 08_Lists_Basics_-_Exercise /07_easter_gifts.py
+++ 08_Lists_Basics_-_Exercise /07_easter_gifts.py
@@ -10,3 +10,31 @@
 # In the end, print the gifts on a single line, except the ones with the value "None",
 # separated by a single space in the following format:
 # "{gift1} {gift2} {gift3} … {giftn}"
+
+gifts = input().split(" ")
+commands = ['OutOfStock', 'Required', 'JustInCase', 'No Money']
+not_finished = True
+
+while not_finished:
+    command = input()
+    if command != "No Money":
+        command_atr = command.split(" ")
+        if command_atr[0] == 'OutOfStock':
+            gifts = ["None" if item == command_atr[1] in gifts else item for item in gifts]
+        elif command_atr[0] == "Required":
+            index = int(command_atr[2])
+            if (len(gifts) - 1) >= index:
+                gifts[index] = command_atr[1]
+            else:
+                continue
+        elif command_atr[0] == "JustInCase":
+            gifts[-1] = command_atr[1]
+    elif command == "No Money":
+        not_finished = False
+
+for word in gifts:
+    if word == "None":
+        gifts.remove("None")
+
+gifts_str = " ".join(gifts)
+print(gifts_str)
