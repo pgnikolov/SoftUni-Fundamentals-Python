7168135b4088454b8d35150d7392c02bcb8ef56a
pgnikolov <151896883+pgnikolov@users.noreply.github.com>
2024-04-14 19:11:26 +0200

    Update 02_center_point.py

--- 12_Functions_More_Exercises /02_center_point.py
+++ 12_Functions_More_Exercises /02_center_point.py
@@ -7,9 +7,9 @@ import math
 
 def closest_point(x1, y1, x2, y2):
     if x1 ** 2 + y1 **2 <= x2 ** 2 + y2 ** 2:           # math.sqrt deosn't work with negative numbers give Value Error
-        print(f"({math.floor(x1)}, {math.floor(y1)})")
+        return f"({math.floor(x1)}, {math.floor(y1)})"
     else:
-        print(f"({math.floor(x2)}, {math.floor(y2)})")
+        return f"({math.floor(x2)}, {math.floor(y2)})"
 
 
 x1 = float(input())
