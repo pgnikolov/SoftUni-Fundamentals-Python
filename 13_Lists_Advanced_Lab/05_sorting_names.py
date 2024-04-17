c54edbc9b831f77bf754f7530ac71bbb5b922a2a
pgnikolov <151896883+pgnikolov@users.noreply.github.com>
2024-04-17 15:55:56 +0200

    Create 05_sorting_names.py

---
+++ 13_Lists_Advanced_Lab/05_sorting_names.py
@@ -0,0 +1,9 @@
+# Write a program that reads a single string with names separated by comma and space ", ".
+# Sort the names by their length in descending order. If 2 or more names have the same length,
+# sort them in ascending order (alphabetically). Print the resulting list.
+
+names = input().split(", ")
+
+names_sort = sorted(names, key=lambda x: (-len(x), x))
+
+print(names_sort)
