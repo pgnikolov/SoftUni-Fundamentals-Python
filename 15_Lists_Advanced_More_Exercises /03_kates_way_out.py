6c5a5c0ca117cce7330b04cc5bc812abc5a44536
pgnikolov <151896883+pgnikolov@users.noreply.github.com>
2024-05-02 19:42:10 +0200

    Create 03_kates_way_out.py

---
+++ 15_Lists_Advanced_More_Exercises /03_kates_way_out.py
@@ -0,0 +1,10 @@
+# On the first line, you will be given how many rows there are in the maze. On the following n lines,
+# you will be given the maze itself. Here is a legend for the maze:
+#     • "#" - means a wall; Kate cannot go through there
+#     • " " - means empty space; Kate can go through there
+#     • "k" - the initial position of Kate; start looking for a way out from there
+#  are two options: Kate either gets out or not:
+#     • If Kate can get out, print the following:
+# "Kate got out in {number_of_moves} moves".
+# Note: If there are two or more ways out, she always chooses the longest one.
+#     • Otherwise, print: "Kate cannot get out".
