3c39c60062f72d91cbe4103b9b4a3980643ad2b1
pgnikolov <151896883+pgnikolov@users.noreply.github.com>
2024-05-02 15:57:56 +0200

    Create 01_social_distribution.py

---
+++ 15_Lists_Advanced_More_Exercises /01_social_distribution.py
@@ -0,0 +1,6 @@
+# On the first line, you will be given the population (numbers separated by comma and space ", ").
+# On the second line, you will be given the minimum wealth. You should distribute the wealth so that no part
+# of the population has less than the minimum wealth. To do that, you should always take wealth
+# from the wealthiest part of the population.
+# There will be cases where the distribution will not be possible.
+# In that case, print: "No equal distribution possible".
