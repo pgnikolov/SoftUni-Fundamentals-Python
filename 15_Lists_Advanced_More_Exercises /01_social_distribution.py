aac2d01297022ec8cb0862ed754527d2892fdd7f
pgnikolov <151896883+pgnikolov@users.noreply.github.com>
2024-05-02 17:27:13 +0200

    Update 01_social_distribution.py

--- 15_Lists_Advanced_More_Exercises /01_social_distribution.py
+++ 15_Lists_Advanced_More_Exercises /01_social_distribution.py
@@ -4,3 +4,28 @@
 # from the wealthiest part of the population.
 # There will be cases where the distribution will not be possible.
 # In that case, print: "No equal distribution possible".
+
+population = [int(char) for char in input().split(", ")]
+
+min_wealth = int(input())
+
+needed_wealth = 0
+available_wealth = 0
+
+for i in population:
+    if i < min_wealth:
+        needed_wealth += min_wealth - i
+    elif i > min_wealth:
+        available_wealth += i - min_wealth
+
+
+if needed_wealth > available_wealth:
+    print("No equal distribution possible")
+else:
+    max_index = population.index(max(population))
+    for j in range(len(population)):
+        max_index = population.index(max(population))
+        if population[j] < min_wealth < population[max_index]:
+            population[max_index] -= (min_wealth - population[j])
+            population[j] += (min_wealth - population[j])
+    print(population)
