cc9ab305ed7def90bb5156f6ca1346d8c0452468
pgnikolov <151896883+pgnikolov@users.noreply.github.com>
2024-05-16 16:28:25 +0200

    Update 07_patterns.py

--- 01_Basic_Syntax_Conditional_Statements_and_Loops/07_patterns.py
+++ 01_Basic_Syntax_Conditional_Statements_and_Loops/07_patterns.py
@@ -1,4 +1,3 @@
-num = int(input())
 
 # for i in range(1, num + 1):
 #     for j in range(0, i):
@@ -10,8 +9,9 @@ num = int(input())
 #         print("*", end="")
 #     print()
 
+num = int(input())
 
-for i in range(1, num + 1):
+for i in range(1, num):
     print("*" * i)
 
 for i in range(num, 0, -1):
