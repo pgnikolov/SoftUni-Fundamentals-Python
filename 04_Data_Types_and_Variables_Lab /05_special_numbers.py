af8cb050598a23f7c2cc0a0f5325343dea1f820a
pgnikolov <151896883+pgnikolov@users.noreply.github.com>
2024-05-22 18:36:02 +0200

    Update 05_special_numbers.py

--- 04_Data_Types_and_Variables_Lab /05_special_numbers.py
+++ 04_Data_Types_and_Variables_Lab /05_special_numbers.py
@@ -2,15 +2,24 @@
 # print the number and if it is special or not (True / False).
 # A number is special when the sum of its digits is 5, 7, or 11.
 
-n = int(input())
+# n = int(input())
+#
+# for i in range(1, n + 1):
+#     sum_of_digits = 0
+#     digits = i
+#     while digits > 0:
+#         sum_of_digits += digits % 10
+#         digits = int(digits / 10)
+#     if (sum_of_digits == 5) or (sum_of_digits == 7) or (sum_of_digits == 11):
+#         print(f'{i} -> True')
+#     else:
+#         print(f'{i} -> False')
 
-for i in range(1, n + 1):
-    sum_of_digits = 0
-    digits = i
-    while digits > 0:
-        sum_of_digits += digits % 10
-        digits = int(digits / 10)
-    if (sum_of_digits == 5) or (sum_of_digits == 7) or (sum_of_digits == 11):
-        print(f'{i} -> True')
+
+n = int(input())
+for i in range(1, n+1):
+    digit_sum = sum(int(digit) for digit in str(i))
+    if digit_sum in [5, 7, 11]:
+        print(f"{i} -> True")
     else:
-        print(f'{i} -> False')
+        print(f"{i} -> False")
