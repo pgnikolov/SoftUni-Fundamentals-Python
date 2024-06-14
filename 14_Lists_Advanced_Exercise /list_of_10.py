3d3937e0908b651ff3854d51869ef0b3d7cb49dd
plamen <pgnikolov@gmail.com>
2024-06-14 19:00:32 +0200

    one more solution added

--- 14_Lists_Advanced_Exercise /list_of_10.py
+++ 14_Lists_Advanced_Exercise /list_of_10.py
@@ -1,20 +1,17 @@
-list_of_number = input().split(", ")
+list_of_numbers = input().split(", ")
 all_lists = []
 working_group = 0
-working_list = list_of_number.copy()
-while len(list_of_number) >= 1:
+working_list = list_of_numbers.copy()
+
+while len(list_of_numbers) >= 1:
     working_group += 10
-    all_lists += [[]]
-    for index_number in range(len(list_of_number)):
-
-        int_number = int(list_of_number[index_number])
-
-        if int_number <= working_group:
-            all_lists[working_group//10 - 1].append(list_of_number[index_number])
-            working_list.remove(list_of_number[index_number])
-    else:
-        list_of_number = working_list.copy()
+    all_lists.append([])
+    for index_number in range(len(list_of_numbers)):
+        int_number = int(list_of_numbers[index_number])
+        if working_group - 10 < int_number <= working_group:
+            all_lists[-1].append(int_number)
+            working_list.remove(list_of_numbers[index_number])
+    list_of_numbers = working_list.copy()
 
 for group in range(len(all_lists)):
-    all_lists[group] = [int(number) for number in all_lists[group]]
     print(f"Group of {(group+1) * 10}'s: {all_lists[group]}")
