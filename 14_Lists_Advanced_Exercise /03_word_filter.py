b20dee959de382fbcb685cd4b11333376ef260cb
plamen <pgnikolov@gmail.com>
2024-06-14 18:20:44 +0200

    one more solution added

--- 14_Lists_Advanced_Exercise /03_word_filter.py
+++ 14_Lists_Advanced_Exercise /03_word_filter.py
@@ -1,7 +1,7 @@
 # Using comprehension, write a program that receives some text, separated by space, and takes only
 # those words whose length is even. Print each word on a new line.
 
-words = [word for word in  input().split(" ") if len(word) % 2 == 0]
+words = [word for word in input().split(" ") if len(word) % 2 == 0]
 
 for word in words:
     print(word)
