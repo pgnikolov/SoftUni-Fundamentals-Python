11a6c558a90f9e4218f7769f67986661b467d408
plamen <pgnikolov@gmail.com>
2024-06-14 18:23:48 +0200

    one more solution added

--- 14_Lists_Advanced_Exercise /03_word_filter.py
+++ 14_Lists_Advanced_Exercise /03_word_filter.py
@@ -2,6 +2,4 @@
 # those words whose length is even. Print each word on a new line.
 
 words = [word for word in input().split(" ") if len(word) % 2 == 0]
-
-for word in words:
-    print(word)
+print('\n'.join(words))
