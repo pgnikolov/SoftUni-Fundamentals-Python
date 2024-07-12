093cc7daf86215a43ad48a7bfab07c549ab2983b
plamen <pgnikolov@gmail.com>
2024-07-12 19:56:10 +0200

    text processing

--- 23_Text_Processing_Exercises /06_replace_repeating_chars.py
+++ 23_Text_Processing_Exercises /06_replace_repeating_chars.py
@@ -1,8 +1,8 @@
-main_string = input()
-
-
-for pos, letter in enumerate(range(len(main_string) - 1), 1):
-    if main_string[letter] != main_string[pos]:
-        print(main_string[letter], end="")
-
-print(main_string[-1])
+some_string = input()
+final_string = ""
+last_added_character = ""
+for current_character in some_string:
+    if current_character != last_added_character:
+        final_string += current_character
+        last_added_character = current_character
+print(final_string)
